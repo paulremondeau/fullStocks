ab424804aafb0faed35a493cb0167f05ec1a0dd7
paulremondeau <poweraider91@gmail.com>
2023-05-30 17:08:52 +0200

    app with database

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -6,23 +6,21 @@ from typing import List, Tuple, Dict
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
-from config import API_KEY
 
 URL_TIMESERIES = "https://api.twelvedata.com/time_series"
 PARAMS_TIMESERIES = {
-    "apikey": API_KEY,
     "interval": "8h",
     "outputsize": "5000",
     "format": "json",
 }
 
 URL_SYMBOLS = "https://api.twelvedata.com/stocks"
-PARAMS_SYMBOLS = {"apikey": API_KEY, "format": "json"}
+PARAMS_SYMBOLS = {"format": "json"}
 
 
 def request_stock_time_series(
-    symbol: str,
-) -> Tuple[Dict[str, float], Dict[str, Dict[str, float]]]:
+    symbol: str, api_key: str
+) -> Tuple[str, Dict[str, float], Dict[str, Dict[str, float]]]:
     """Request the twelve data API for stock informations.
 
     This function requests meta and time series for the requested
@@ -36,7 +34,8 @@ def request_stock_time_series(
 
     Returns
     -------
-    Tuple[Dict, Dict[str, Dict[str, float]]]
+    Tuple[str, Dict, Dict[str, Dict[str, float]]]
+        The str is the twelve data API call status.
         The first dictionnary is the time series data.
         The second dictionnary contains the statistics data.
     """
@@ -44,63 +43,42 @@ def request_stock_time_series(
     # API request
     params = copy(PARAMS_TIMESERIES)
     params["symbol"] = symbol
+    params["apikey"] = api_key
     response = requests.get(URL_TIMESERIES, params=params)
 
-    # Convert to dataframe for easier manipulation
-    df = pd.DataFrame(response.json()["values"])
-    df = df.astype(
-        {
-            "open": "float64",
-            "high": "float64",
-            "high": "float64",
-            "close": "float64",
-            "volume": "int64",
-        }
-    )
-    df["datetime"] = pd.to_datetime(df["datetime"])
-    df = df.set_index("datetime").sort_index()
+    status = response.json()["status"]
 
-    working_df: pd.Series = df["close"]
-    working_df = working_df.rename(symbol)
+    if status == "error":
+        # The request failed
+        return status, None
 
-    # Cumulative return as (f(t) - f(t-1))/f(t-1) *100
-    cumulative_return: float = (
-        (working_df.iloc[-1] - working_df.iloc[-2]) / working_df.iloc[-2] * 100
-    )
-    cumulative_return = float(f"{cumulative_return:.2f}")
+    elif status == "ok":
+        # Convert to dataframe for easier manipulation
+        df = pd.DataFrame(response.json()["values"])
+        df = df.astype(
+            {
+                "open": "float64",
+                "high": "float64",
+                "high": "float64",
+                "close": "float64",
+                "volume": "int64",
+            }
+        )
+        df["datetime"] = pd.to_datetime(df["datetime"])
+        df = df.set_index("datetime").sort_index()
 
-    # Annualized cumulative return
-    stock_price_one_year_ago: float = working_df[
-        working_df.index >= (working_df.index[-1] - relativedelta(years=1))
-    ].iloc[0]
-    annualized_cumulative_return: float = (
-        (working_df.iloc[-1] - stock_price_one_year_ago)
-        / stock_price_one_year_ago
-        * 100
-    )
-    annualized_cumulative_return = float(f"{annualized_cumulative_return:.2f}")
+        working_df: pd.Series = df["close"]
 
-    # Annualized volatility
-    annualized_volatility: float = (
-        working_df[working_df.index >= working_df.index[-1] - relativedelta(years=1)]
-    ).std()
-    annualized_volatility = float(f"{annualized_volatility:.2f}")
+        # # Performance
+        # performance_df = working_df / working_df.iloc[0] * 100
+        # performance_json: Dict[str, float] = json.loads(working_df.to_json())
 
-    json_stats: Dict[str, Dict[str, float]] = {
-        "symbol": symbol,
-        "cumulativeReturn": cumulative_return,
-        "annualizedCumulativeReturn": annualized_cumulative_return,
-        "annualizedVolatility": annualized_volatility,
-    }
-
-    # Performance
-    performance_df = working_df / working_df.iloc[0] * 100
-    performance_json: Dict[str, float] = json.loads(performance_df.to_json())
-
-    return performance_json, json_stats
+        return status, working_df
 
 
-def request_avalaible_symbols(exchange: List[str] = ["NASDAQ"]) -> List[str]:
+def request_avalaible_symbols(
+    api_key: str, exchange: List[str] = ["NASDAQ"]
+) -> List[str]:
     """Fetch the available symbols list.
 
     This function fetch the twelvedata API to get
@@ -120,7 +98,106 @@ def request_avalaible_symbols(exchange: List[str] = ["NASDAQ"]) -> List[str]:
     params = copy(PARAMS_SYMBOLS)
     if exchange:
         params["exchange"] = exchange
+    params["apikey"] = api_key
     response = requests.get(URL_SYMBOLS, params=params)
 
     symbols_list: List[str] = [x["symbol"] for x in response.json()["data"]]
     return symbols_list
+
+
+def format_sending_data(
+    stock_dates: List[pd.Timestamp] | None,
+    stock_time_series: List[float] | None,
+    performance: bool = True,
+) -> List[list]:
+    """Format data to send to the frontend.
+
+    This function pre-process the data before sending to
+    the front-end. The goal is to match the apexcharts line chart
+    format, where a point is [12345, 1.0].
+
+    Parameters
+    ----------
+    stock_dates : List[pd.Timestamp] | None
+        The timestamps data.
+    stock_time_series : List[float] | None
+        The stock values data.
+    performance : bool, optional
+        If true, transforms data to create performance data, by
+        default True
+
+    Returns
+    -------
+    List[list] | None
+        The data formatted.
+    """
+    if stock_dates is None or stock_time_series is None:
+        return None
+
+    else:
+        stocks_date_timestamps = [
+            int(date_value.timestamp() * 1000) for date_value in stock_dates
+        ]
+
+        if performance:
+            stock_time_series = list(
+                pd.Series(stock_time_series) / stock_time_series[0] * 100
+            )
+
+        result = [
+            [timestamp, float(f"{ts_value:.2f}")]
+            for timestamp, ts_value in zip(stocks_date_timestamps, stock_time_series)
+        ]
+
+        return result
+
+
+def evaluate_stats_information(data: pd.Series, symbol: str) -> Dict[str, float | str]:
+    """Gives several statistics about stock time-series.
+
+    This function evaluates several statistics about a
+    stock time-series. The statistics are:
+    - cumulative return
+    - annualized cumulative return
+    - annualized volatility
+
+    Parameters
+    ----------
+    data : pd.Series
+        The data, sorted in ascending time.
+
+    symbol : str
+        The stock symbol
+
+    Returns
+    -------
+    Dict[str, float | str]
+        The result dict.
+    """
+    # Cumulative return as (f(t) - f(t-1))/f(t-1) *100
+    cumulative_return: float = (data.iloc[-1] - data.iloc[-2]) / data.iloc[-2] * 100
+    cumulative_return = float(f"{cumulative_return:.2f}")
+
+    # Annualized cumulative return
+    stock_price_one_year_ago: float = data[
+        data.index >= (data.index[-1] - relativedelta(years=1))
+    ].iloc[0]
+    annualized_cumulative_return: float = (
+        (data.iloc[-1] - stock_price_one_year_ago) / stock_price_one_year_ago * 100
+    )
+    annualized_cumulative_return = float(f"{annualized_cumulative_return:.2f}")
+
+    # Annualized volatility
+    annualized_volatility: float = (
+        data[data.index >= data.index[-1] - relativedelta(years=1)]
+    ).std()
+    annualized_volatility = float(f"{annualized_volatility:.2f}")
+
+    json_stats: Dict[str, Dict[str, float]] = {
+        "symbol": symbol,
+        "cumulativeReturn": cumulative_return,
+        "annualizedCumulativeReturn": annualized_cumulative_return,
+        "annualizedVolatility": annualized_volatility,
+    }
+
+    return json_stats
