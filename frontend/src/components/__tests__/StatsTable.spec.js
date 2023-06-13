ff419dc09a854036f06b7658ac2a856e60c0559b
paulremondeau <poweraider91@gmail.com>
2023-06-13 16:38:17 +0200

    update stats table test

--- frontend/src/components/__tests__/StatsTable.spec.js
+++ frontend/src/components/__tests__/StatsTable.spec.js
@@ -1,31 +1,31 @@
 import { describe, it, expect } from 'vitest'
 
-import {reactive} from 'vue'
+import { reactive } from 'vue'
 import { mount } from '@vue/test-utils'
 import StatsTable from '../StatsTable.vue'
 
 describe('StatsTable', () => {
-  
-    it ('initial render is empty', () => {
-        
-        const wrapper = mount(StatsTable, 
-            { 
-                props: 
+
+    it('initial render is empty', () => {
+
+        const wrapper = mount(StatsTable,
+            {
+                props:
                 {
                     tableData: []
                 }
             }
         )
-        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized comulative returnAnnualized volatilityNo data")
+        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized cumulative returnAnnualized volatilityNo data")
 
     })
 
     it('it renders data properly', () => {
-        
-        const wrapper = mount(StatsTable, 
+
+        const wrapper = mount(StatsTable,
             {
                 props: {
-    
+
                     tableData: [{
                         "symbol": "TEST",
                         "cumulativeReturn": 10,
@@ -38,27 +38,27 @@ describe('StatsTable', () => {
                         "annualizedCumulativeReturn": 55,
                         "annualizedVolatility": 23,
                     }]
-                }   
+                }
             }
         )
 
-        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized comulative returnAnnualized volatilityTEST101520ABCD905523Showing 1-2 of 2Row count:102550Go to page:1«First<Prev1>Next»Last")
+        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized cumulative returnAnnualized volatilityTEST101520ABCD905523Showing 1-2 of 2Row count:102550Go to page:1«First<Prev1>Next»Last")
     })
 
     it('it updates data properly', async () => {
         // No data at first
         const tableData = reactive([])
 
-        const wrapper = mount(StatsTable, 
-            { 
-                props: 
+        const wrapper = mount(StatsTable,
+            {
+                props:
                 {
                     tableData: tableData
                 }
             }
         )
 
-        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized comulative returnAnnualized volatilityNo data")
+        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized cumulative returnAnnualized volatilityNo data")
 
         // Add some data
         await tableData.push(...[
@@ -75,13 +75,13 @@ describe('StatsTable', () => {
                 "annualizedVolatility": 23,
             }
         ])
-            
 
-        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized comulative returnAnnualized volatilityTEST101520ABCD905523Showing 1-2 of 2Row count:102550Go to page:1«First<Prev1>Next»Last")
+
+        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized cumulative returnAnnualized volatilityTEST101520ABCD905523Showing 1-2 of 2Row count:102550Go to page:1«First<Prev1>Next»Last")
 
         // Remove data
         await tableData.pop()
-        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized comulative returnAnnualized volatilityTEST101520Showing 1-1 of 1Row count:102550Go to page:1«First<Prev1>Next»Last")
+        expect(wrapper.text()).toBe("SymbolCumulative returnAnnualized cumulative returnAnnualized volatilityTEST101520Showing 1-1 of 1Row count:102550Go to page:1«First<Prev1>Next»Last")
 
     })
 })
